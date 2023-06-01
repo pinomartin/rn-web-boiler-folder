b47f0bcb414e1aead2123660f423727a2bd8ebad
pinomartin <martin91pino@gmail.com>
2023-06-01 12:00:48 -0300

    initial commit

---
+++ local_modules/react-native-reanimated/index.js
@@ -0,0 +1,11 @@
+ 
+export const Bubbles ={};
+export const DoubleBounce = {};
+export const Bars ={};
+export const Pulse ={};
+
+
+export default {}
+
+
+//yarn add ./local_modules/react-native-reanimated --ignore-engines
